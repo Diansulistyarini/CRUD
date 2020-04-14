572d68feb99ced110b9bdd99c57439f6b62a323f
Diansulistyarini <55771328+Diansulistyarini@users.noreply.github.com>
2020-04-14 19:53:59 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,8 +3,7 @@
 try {
     $con = new PDO("mysql:host=localhost;dbname=test1","root","");
     // echo "Database Terhubung";
-    //INSERT INTO `food` (`kd_menu`, `nama_produk`, `harga_produk`, `satuan`, `stok`) 
-    //VALUES (NULL, 'Masker', '5000', 'pcs', '100');
+    
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
